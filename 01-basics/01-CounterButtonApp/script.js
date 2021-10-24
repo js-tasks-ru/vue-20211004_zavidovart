fd7b5062c9dec502fdbc0ad97ed74cb7e4f25bfb
artem.zavidov <artem.zavidov@cherehapa.ru>
2021-10-24 22:49:04 +0300

    CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,25 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const Root = defineComponent({
+  el: `#app`,
+
+  name: 'App',
+
+  data() {
+    return {
+      value: 0,
+    };
+  },
+
+  methods: {
+    addOne() {
+      this.value = this.value + 1;
+    }
+  }
+});
+
+const app = createApp(Root);
+
+const vm = app.mount('#app');
+
+window.vm = vm;
